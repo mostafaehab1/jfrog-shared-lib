42b08cb0e61ee43a0d3a3f2b59d2b9c3c77432d6
Super User <root@localhost.localdomain>
2026-09-21 19:39:11 +0300

    Upload frontend artifact to JFrog

--- vars/uploadFrontend.groovy
+++ vars/uploadFrontend.groovy
@@ -3,6 +3,7 @@ def call(Map config = [:]) {
     def distDir = config.distDir ?: 'dist'
     def repository = config.repository ?: 'key'
     def target = config.target ?: env.BUILD_NUMBER
+    def archive = "frontend-dist-${env.BUILD_NUMBER}.tar.gz"
 
     echo "Uploading frontend build..."
     echo "Dist directory: ${distDir}"
@@ -10,8 +11,23 @@ def call(Map config = [:]) {
     echo "Target: ${target}"
 
     sh """
-        tar -czf frontend-dist-${env.BUILD_NUMBER}.tar.gz -C ${distDir} .
-
-        ls -lh frontend-dist-${env.BUILD_NUMBER}.tar.gz
+        tar -czf ${archive} -C ${distDir} .
+        ls -lh ${archive}
     """
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'artifactory-credentials',
+            usernameVariable: 'JFROG_USER',
+            passwordVariable: 'JFROG_PASSWORD'
+        )
+    ]) {
+        sh """
+            curl -f -u "\$JFROG_USER:\$JFROG_PASSWORD" \
+              -T "${archive}" \
+              "http://192.168.44.129:8081/artifactory/${repository}/${target}/${archive}"
+        """
+    }
+
+    echo "Upload completed successfully."
 }
